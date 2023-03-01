39db9b605307f1db31267dc2c3303eab338c2388
Amir <amir.boisov@gmail.com>
2023-03-01 15:17:12 +0300

    test

--- app.js
+++ app.js
@@ -61,124 +61,120 @@ toDubki[0].addEventListener("click", () => {
 // Get timetable
 
 function getTimetable1(dayOfWeek) {
-    setTimeout(() => {
-        let date = new Date();
-        let dateHours = date.getHours();
-        let dateMinutes = date.getMinutes();
-        let output;
-        // Filtration works
-        dayOfWeek = dayOfWeek.filter((d) => {
-            return d.whereTo == "toMoscow";
-        });
+    let date = new Date();
+    let dateHours = date.getHours();
+    let dateMinutes = date.getMinutes();
+    let output;
+    // Filtration works
+    dayOfWeek = dayOfWeek.filter((d) => {
+        return d.whereTo == "toMoscow";
+    });
 
-        dayOfWeek.forEach((weekDay) => {
-            let tempDifference = 0;
-            let difference;
-            let tempHours = weekDay.hours;
-            let tempMinutes = weekDay.minutes;
-            if (tempHours != "") {
-                tempDifference = tempHours * 60 + tempMinutes - dateHours * 60 - dateMinutes;
-            } else {
-                tempDifference = undefined;
-            }
+    dayOfWeek.forEach((weekDay) => {
+        let tempDifference = 0;
+        let difference;
+        let tempHours = weekDay.hours;
+        let tempMinutes = weekDay.minutes;
+        if (tempHours != "") {
+            tempDifference = tempHours * 60 + tempMinutes - dateHours * 60 - dateMinutes;
+        } else {
+            tempDifference = undefined;
+        }
 
-            let formatHours;
+        let formatHours;
 
-            if (tempDifference != undefined) {
-                formatHours = Math.floor(tempDifference / 60);
-            } else {
-                formatHours = undefined;
-            }
-            switch (true) {
-                case formatHours > 0:
-                    difference = `${Math.floor(tempDifference / 60)}ч ${tempDifference % 60}мин`;
-                    break;
-                case formatHours == 0:
-                    difference = `${tempDifference % 60} мин`;
-                    break;
-                case formatHours < 0:
-                    difference = `${Math.abs(Math.floor(tempDifference / 60))}ч ${Math.abs(
-                        tempDifference % 60,
-                    )} мин. назад`;
-                    break;
-                case formatHours == undefined:
-                    difference = "По прибытию";
-                    break;
-            }
+        if (tempDifference != undefined) {
+            formatHours = Math.floor(tempDifference / 60);
+        } else {
+            formatHours = undefined;
+        }
+        switch (true) {
+            case formatHours > 0:
+                difference = `${Math.floor(tempDifference / 60)}ч ${tempDifference % 60}мин`;
+                break;
+            case formatHours == 0:
+                difference = `${tempDifference % 60} мин`;
+                break;
+            case formatHours < 0:
+                difference = `${Math.abs(Math.floor(tempDifference / 60))}ч ${Math.abs(
+                    tempDifference % 60,
+                )} мин. назад`;
+                break;
+            case formatHours == undefined:
+                difference = "По прибытию";
+                break;
+        }
 
-            if (formatHours >= 0) {
-                output += `<div style="background-color:${weekDay.color}" class="card">
+        if (formatHours >= 0) {
+            output += `<div style="background-color:${weekDay.color}" class="card">
             <div class="directionAndTimeLeft">
                 <div class="direction">${weekDay.direction}</div>
                 <div class="timeLeft">${difference}</div>
             </div>
             <div class="time">${weekDay.time}</div>
         </div>`;
-            } else {
-                output = "";
-            }
-        });
-        timetableSection.innerHTML = output;
-    }, 100);
+        } else {
+            output = "";
+        }
+    });
+    timetableSection.innerHTML = output;
 }
 
 function getTimetable2(dayOfWeek) {
-    setTimeout(() => {
-        let date = new Date();
-        let dateHours = date.getHours();
-        let dateMinutes = date.getMinutes();
-        let output = "";
+    let date = new Date();
+    let dateHours = date.getHours();
+    let dateMinutes = date.getMinutes();
+    let output = "";
 
-        dayOfWeek.forEach((weekDay) => {
-            let tempDifference = 0;
-            let difference;
-            let tempHours = weekDay.hours;
-            let tempMinutes = weekDay.minutes;
-            if (tempHours != "") {
-                tempDifference = tempHours * 60 + tempMinutes - dateHours * 60 - dateMinutes;
-            } else {
-                tempDifference = undefined;
-            }
+    dayOfWeek.forEach((weekDay) => {
+        let tempDifference = 0;
+        let difference;
+        let tempHours = weekDay.hours;
+        let tempMinutes = weekDay.minutes;
+        if (tempHours != "") {
+            tempDifference = tempHours * 60 + tempMinutes - dateHours * 60 - dateMinutes;
+        } else {
+            tempDifference = undefined;
+        }
 
-            let formatHours;
+        let formatHours;
 
-            if (tempDifference != undefined) {
-                formatHours = Math.floor(tempDifference / 60);
-            } else {
-                formatHours = undefined;
-            }
-            switch (true) {
-                case formatHours > 0:
-                    difference = `${Math.floor(tempDifference / 60)}ч ${tempDifference % 60}мин`;
-                    break;
-                case formatHours == 0:
-                    difference = `${tempDifference % 60} мин`;
-                    break;
-                case formatHours < 0:
-                    difference = `${Math.abs(Math.floor(tempDifference / 60))}ч ${Math.abs(
-                        tempDifference % 60,
-                    )} мин. назад`;
-                    break;
-                case formatHours == undefined:
-                    difference = "По прибытию";
-                    break;
-            }
-            console.log(formatHours);
+        if (tempDifference != undefined) {
+            formatHours = Math.floor(tempDifference / 60);
+        } else {
+            formatHours = undefined;
+        }
+        switch (true) {
+            case formatHours > 0:
+                difference = `${Math.floor(tempDifference / 60)}ч ${tempDifference % 60}мин`;
+                break;
+            case formatHours == 0:
+                difference = `${tempDifference % 60} мин`;
+                break;
+            case formatHours < 0:
+                difference = `${Math.abs(Math.floor(tempDifference / 60))}ч ${Math.abs(
+                    tempDifference % 60,
+                )} мин. назад`;
+                break;
+            case formatHours == undefined:
+                difference = "По прибытию";
+                break;
+        }
+        console.log(formatHours);
 
-            if (formatHours >= 0) {
-                output += `<div style="background-color:${weekDay.color}" class="card">
+        if (formatHours >= 0) {
+            output += `<div style="background-color:${weekDay.color}" class="card">
             <div class="directionAndTimeLeft">
                 <div class="direction">${weekDay.direction}</div>
                 <div class="timeLeft">${difference}</div>
             </div>
             <div class="time">${weekDay.time}</div>
         </div>`;
-            } else {
-                output = "";
-            }
-        });
-        timetableSection.innerHTML = output;
-    }, 100);
+        } else {
+            output = "";
+        }
+    });
+    timetableSection.innerHTML = output;
 }
 
 calculateDayOfWeek1();
